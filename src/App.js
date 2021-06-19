04009f6308bf33eaac34798065c6db1865a46adc
MuthuramanChockalingam <ramsmart710@gmail.com>
2021-06-19 08:44:08 +0000

    Corrected position and added animation rotation.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 
 const App = () =>
 	<div className="App">
-		<div className="transform">
-			<div className="box b1 shadow"> </div>
-			<div className="box b2 shadow"> </div>
-			<div className="box b3 shadow"> </div>
-			<div className="box b4 shadow"> </div>
+		<div className="container">
+			<div className="box b1"> </div>
+			<div className="box b2"> </div>
+			<div className="box b3"> </div>
+			<div className="box b4"> </div>
 		</div>
 	</div>
 	;
