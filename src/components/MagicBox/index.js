57922a4bba71dcee5a8491f38fa60dbac58cc668
MuthuramanChockalingam <ramsmart710@gmail.com>
2021-07-01 09:11:47 +0000

    linted.

--- src/components/MagicBox/index.js
+++ src/components/MagicBox/index.js
@@ -1,17 +1,17 @@
 import { React } from 'react';
 import './index.css';
-import config from "../../core/config";
+import config from '../../core/config';
 
 const { count } = config.magicBox;
-const randomNumber = Math.floor(Math.random() * count) + 1
+const randomNumber = Math.floor(Math.random() * count) + 1;
+const two = 2;
 
 const Box = (num) =>
-	<div className = { `box ${num % 2 ? 'light' : 'dark'}` }>
+	<div className={ `box ${ num % two ? 'light' : 'dark' }` }>
 		{ num > 1 ? Box(num - 1) : randomNumber }
 	</div>;
 
 const MagicBox = () =>
-<div className='magic-box'>{ Box(randomNumber) }</div>;
-
+	<div className="magic-box">{ Box(randomNumber) }</div>;
 
 export default MagicBox;
--- src/components/Pendulum/index.js
+++ src/components/Pendulum/index.js
@@ -2,10 +2,10 @@ import { React } from 'react';
 import './index.css';
 
 const Pendulum = () =>
-	<div className='pendulum'>
-		<div className='container'>
-			<div className='line'></div>
-			<div className='ball'></div>
+	<div className="pendulum">
+		<div className="container">
+			<div className="line"/>
+			<div className="ball"/>
 		</div>
 	</div>;
 
